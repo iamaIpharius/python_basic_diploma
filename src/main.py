f38bef59eb54a8e090e357265ebce290e7a36774
Alex <sturmmanngr@gmail.com>
2021-11-02 10:54:37 +0700

    lowprice working

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import telebot
 from decouple import config
+from telebot.types import InputMediaPhoto
+
 from botrequests import lowprice
 import sqlite3
 
@@ -17,9 +19,9 @@ def insert_row(value):
     conn.commit()
 
 
-def update_db(value):
+def update_db(value, column):
     user = 'user' + str(value.from_user.id)
-    c.execute(f"""UPDATE {user} SET city = "{str(value.text)}" WHERE rowid = (SELECT MAX(rowid) FROM {user})""")
+    c.execute(f"""UPDATE {user} SET {column} = "{str(value.text)}" WHERE rowid = (SELECT MAX(rowid) FROM {user})""")
     conn.commit()
 
 
@@ -52,47 +54,37 @@ def lowprice_start(message):
 
 
 def where_we_going(message):
-    user = 'user' + str(message.from_user.id)
-    print(user)
-    c.execute(f"""UPDATE {user} SET city = "{str(message.text)}" WHERE rowid = (SELECT MAX(rowid) FROM {user})""")
-    conn.commit()
+    update_db(message, 'city')
+
 
     bot.send_message(message.from_user.id, 'Сколько отелей нужно вывести в поиске? ')
     bot.register_next_step_handler(message, how_many_hotels)
 
 
 def how_many_hotels(message):
+    update_db(message, 'hotels_count')
     user = 'user' + str(message.from_user.id)
-    c.execute(
-        f"""UPDATE {user} SET hotels_count = "{str(message.text)}" WHERE rowid = (SELECT MAX(rowid) FROM {user})""")
-    conn.commit()
     bot.send_message(message.from_user.id, 'Дата заезда в формате yyyy-MM-dd: ')
     bot.register_next_step_handler(message, set_check_in)
 
 
 def set_check_in(message):
-    user = 'user' + str(message.from_user.id)
-    c.execute(f"""UPDATE {user} SET check_in = "{message.text}" WHERE rowid = (SELECT MAX(rowid) FROM {user})""")
-    conn.commit()
+    update_db(message, 'check_in')
     bot.send_message(message.from_user.id, 'Дата выезда в формате yyyy-MM-dd: ')
     bot.register_next_step_handler(message, set_check_out)
 
 
 def set_check_out(message):
-    user = 'user' + str(message.from_user.id)
-    c.execute(f"""UPDATE {user} SET check_out = "{message.text}" WHERE rowid = (SELECT MAX(rowid) FROM {user})""")
-    conn.commit()
+    update_db(message, 'check_out')
     bot.send_message(message.from_user.id, 'Нужны ли фотографии? (да/нет) ')
     bot.register_next_step_handler(message, need_photos)
 
 
 def need_photos(message):
-    user = 'user' + str(message.from_user.id)
-    c.execute(f"""UPDATE {user} SET photos = "{str(message.text)}" WHERE rowid = (SELECT MAX(rowid) FROM {user})""")
-    conn.commit()
-
+    update_db(message, 'photos')
     bot.send_message(message.from_user.id, 'ОБРАБАТЫВАЮ...')
 
+    user = 'user' + str(message.from_user.id)
     c.execute(f"SELECT * FROM {user}")
     table = c.fetchall()
     work_row = table[-1]
@@ -104,11 +96,12 @@ def need_photos(message):
         current_hotels_list = lowprice.hotels_list_by_lowprice(dest_id, work_row[2], work_row[3], work_row[4])
         for hotel in current_hotels_list:
             info_about_hotel = lowprice.form_result_string(hotel)
-            bot.send_message(message.from_user.id, info_about_hotel)
             current_photos_list = lowprice.get_photos(hotel['id'])
             current_photos_list = [x.format(size='b') for x in current_photos_list]
-            for photo_url in current_photos_list:
-                bot.send_message(message.from_user.id, photo_url)
+            media_array = [InputMediaPhoto(x) for x in current_photos_list[:9]]
+            media_array[0].caption = info_about_hotel
+            bot.send_media_group(message.from_user.id, media_array)
+
     else:
         bot.send_message(message.from_user.id, 'ПОДОЖДИТЕ...')
         dest_id = lowprice.get_destination_id(work_row[1])
